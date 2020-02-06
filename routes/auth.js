479ac6be5f2ba96fe2d7639fe4620ad81d3c2ea3
Zenna Liang <zenna.liang@gmail.com>
2020-02-06 17:16:33 -0500

    added forget & reset password w/ resetLink to user schema

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const { signUp, signIn, signOut} = require("../controllers/auth");
-const { findUserById } = require("../controllers/user");
-
-const { userSignupValidator } = require("../validator");
-
 const router = express.Router();
+
+const { findUserById } = require("../controllers/user");
+const { userSignupValidator, passwordResetValidator } = require("../validator");
+const { 
+    signUp, 
+    signIn, 
+    signOut,
+    forgotPassword,
+    resetPassword
+} = require("../controllers/auth");
 
 router.post("/signup", userSignupValidator, signUp);
 router.post("/signin", signIn);
 router.get("/signout", signOut);
 
+// password forgot and reset routes
+router.put("/forgot-password", forgotPassword);
+router.put("/reset-password", passwordResetValidator, resetPassword);
 
 // check if user exist when any route uses :userId in para
 router.param("userId", findUserById);
