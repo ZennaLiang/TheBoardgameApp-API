2fb9a697cbba1a6390180ace86424e1a1f537274
unknown <zenna.liang@gmail.com>
2020-10-22 15:35:38 -0400

    added update,delete event

--- routes/event.js
+++ routes/event.js
@@ -2,18 +2,23 @@ const express = require("express");
 
 const { requireSignIn } = require("../controllers/auth");
 const {
+  findUserById,
   getEvent,
   findEventById,
   eventsByUser,
   createEvent,
+  isOwner,
+  updateEvent,
+  deleteEvent,
 } = require("../controllers/event");
-const { findUserById } = require("../controllers/user");
 
 const router = express.Router();
 router.get("/event/:eventId", getEvent);
 router.get("/events/by/:userId", requireSignIn, eventsByUser);
 
 router.post("/event/new/:userId", requireSignIn, createEvent);
+router.put("/event/:eventId", requireSignIn, isOwner, updateEvent);
+router.delete("/event/:eventId", requireSignIn, isOwner, deleteEvent);
 
 // check if user exist when any route uses :userId in para
 router.param("userId", findUserById);
