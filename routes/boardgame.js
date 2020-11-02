1ed297af5d8abde20e42aa2e4d37b12b7a021cfb
Louie Tse <28274608+tselouie@users.noreply.github.com>
2020-11-02 15:26:30 -0500

    route to update userboardgames

--- routes/boardgame.js
+++ routes/boardgame.js
@@ -7,6 +7,7 @@ const {
   getBoardgame,
   getBGGCounts,
   getUserCollection,
+  updateUserCollection
 } = require("../controllers/boardgame");
 
 const router = express.Router();
@@ -15,6 +16,7 @@ router.get("/boardgame/user/:bggUsername", getUserBggBoardgames);
 router.get("/boardgame/:bggUsername", getBggBoardgames);
 router.get("/boardgame/count/:bggUsername", getBGGCounts);
 router.get("/boardgame/user/collection/:userId", getUserCollection);
+router.post("/boardgame/user/collection/:userId/update", updateUserCollection);
 // any route containing :userId, our app will first execute userByID()
 router.param("userId", findUserById);
 module.exports = router;
