611e952c260a756559aac48df02b8a40d7de4ac8
Zenna Liang <zenna.liang@gmail.com>
2020-12-13 15:21:30 -0500

    find user & update user test

--- test/api/user.test.js
+++ test/api/user.test.js
@@ -7,16 +7,29 @@ const { it } = require("mocha");
 const should = chai.should();
 
 describe("User Controller", () => {
-  let nUser;
-  let token;
+  let regUser;
+  let regUser2;
+  let adminUser;
   before((done) => {
-    nUser = new User({
-      email: "user12@test.com",
+    regUser = new User({
+      email: "user1@test.com",
       name: "janedoe",
       password: "testuser",
     });
-    nUser.save();
-
+    regUser2 = new User({
+      email: "user2@test.com",
+      name: "johndoe",
+      password: "testuser",
+    });
+    adminUser = new User({
+      email: "user3@test.com",
+      name: "sam smith",
+      password: "testuser",
+      role: "admin"
+    });
+    regUser.save();
+    regUser2.save();
+    adminUser.save();
     setTimeout(function () {
       done();
     }, 500);
@@ -35,7 +48,7 @@ describe("User Controller", () => {
         .send()
         .end((err, res) => {
           expect(res).to.have.status(400);
-          expect(res.body).to.have.property("error");
+          expect(res).to.have.nested.property("body.error");
           done();
         });
     });
@@ -45,7 +58,7 @@ describe("User Controller", () => {
         .get("/api/user/find/janedoe")
         .send()
         .end((err, res) => {
-          expect(res.body).to.have.property("user");
+          expect(res).to.have.nested.property("body.user");
           expect(res).to.have.status(200);
           done();
         });
@@ -72,32 +85,123 @@ describe("User Controller", () => {
           done();
         });
     });
-    it("should show status 401 if userId sent by unauthorized user", (done) => {
+    it("should show status 401 if user did not log in", (done) => {
       chai
         .request(app)
-        .get(`/api/user/${nUser._id}`)
+        .get(`/api/user/${regUser._id}`)
         .send()
         .end((err, res) => {
-          expect(res.text).to.equal('{"error":"Unauthorized Access!"}');
+          expect(res).to.have.nested.property(
+            "text",
+            '{"error":"Unauthorized Access!"}'
+          );
           expect(res).to.have.status(401);
           done();
         });
     });
-    it("should show status 200 if userId sent by authorized user", (done) => {
+    it("should show status 200 w/ user info if user log in", (done) => {
       chai
         .request(app)
         .post("/api/signin")
         .send({
-          email: "user12@test.com",
+          email: "user1@test.com",
           password: "testuser",
         })
         .end((err, res) => {
-          token = res.body.token;
+          expect(res).to.have.nested.property("body.token");
           chai
             .request(app)
-            .get(`/api/user/${nUser._id}`)
-            .set("Authorization", `Bearer ${token}`)
+            .get(`/api/user/${regUser._id}`)
+            .set("Authorization", `Bearer ${res.body.token}`)
             .end((err, res) => {
+              expect(res).to.have.nested.property("body.name");
+              expect(res).to.not.have.nested.property("body.hashed_password");
+              expect(res).to.not.have.nested.property("body.salt");
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+  });
+  describe("findAllUsers", () => {
+    it("should return list with 3 users", (done) => {
+      chai
+        .request(app)
+        .get(`/api/users`)
+        .send()
+        .end((err, res) => {
+          expect(res.body).to.have.lengthOf(3);
+          done();
+        });
+    });
+  });
+  describe("updateUser", () => {
+    it("should show 401 if user not logged in", (done) => {
+      chai
+        .request(app)
+        .put(`/api/user/${regUser._id}`)
+        .type("form")
+        .send({
+          about: "hello",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property(
+            "text",
+            '{"error":"Unauthorized Access!"}'
+          );
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
+    it("should show 403 when updated by someone that is not the owner nor admin", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user2@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .put(`/api/user/${regUser._id}`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .type("form")
+            .send({
+              about: "hello",
+            })
+            .end((err, res) => {
+              expect(res).to.have.nested.property(
+                "text",
+                '{"error":"User is not authorized to perform this action"}'
+              );
+              expect(res).to.have.status(403);
+              done();
+            });
+        });
+    });
+    it("should show 200 when updated by admin", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user3@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .put(`/api/user/${regUser._id}`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .set('Accept', 'application/json')
+            .field("about", "here")
+            .end((err, res) => {
+              expect(res).to.have.nested.property(
+                "body.user.about",
+                'here'
+              );
               expect(res).to.have.status(200);
               done();
             });
@@ -105,4 +209,3 @@ describe("User Controller", () => {
     });
   });
 });
-
