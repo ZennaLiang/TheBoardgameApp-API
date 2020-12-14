445cfd307c4ceda80d19387506b87a572528d266
Zenna Liang <zenna.liang@gmail.com>
2020-12-14 16:33:27 -0500

    test - getUserphoto, follow, unfollow, delete user

--- test/api/user.test.js
+++ test/api/user.test.js
@@ -25,7 +25,7 @@ describe("User Controller", () => {
       email: "user3@test.com",
       name: "sam smith",
       password: "testuser",
-      role: "admin"
+      role: "admin",
     });
     regUser.save();
     regUser2.save();
@@ -40,7 +40,7 @@ describe("User Controller", () => {
     done();
   });
 
-  describe("findUserByName", () => {
+  describe("find user by name", () => {
     it("should show status 404 if username not exist", (done) => {
       chai
         .request(app)
@@ -74,7 +74,7 @@ describe("User Controller", () => {
         });
     });
   });
-  describe("getUser", () => {
+  describe("get user", () => {
     it("should show status 400 if no userId sent", (done) => {
       chai
         .request(app)
@@ -123,7 +123,7 @@ describe("User Controller", () => {
         });
     });
   });
-  describe("findAllUsers", () => {
+  describe("find all user", () => {
     it("should return list with 3 users", (done) => {
       chai
         .request(app)
@@ -135,7 +135,7 @@ describe("User Controller", () => {
         });
     });
   });
-  describe("updateUser", () => {
+  describe("update user", () => {
     it("should show 401 if user not logged in", (done) => {
       chai
         .request(app)
@@ -195,12 +195,265 @@ describe("User Controller", () => {
             .request(app)
             .put(`/api/user/${regUser._id}`)
             .set("Authorization", `Bearer ${res.body.token}`)
-            .set('Accept', 'application/json')
+            .set("Accept", "application/json")
             .field("about", "here")
             .end((err, res) => {
+              expect(res).to.have.nested.property("body.user.about", "here");
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+    it("should show 200 when updated by owner", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user1@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .put(`/api/user/${regUser._id}`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .set("Accept", "application/json")
+            .field("about", "here")
+            .end((err, res) => {
+              expect(res).to.have.nested.property("body.user.about", "here");
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+    it("should show 200 when updated by owner", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user1@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .put(`/api/user/${regUser._id}`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .set("Accept", "application/json")
+            .field("about", "here")
+            .attach("photo", "./test/defaultProfile.png")
+            .end((err, res) => {
+              expect(res).to.have.nested.property("body.user.photo");
+              expect(res).to.have.nested.property("body.user.about", "here");
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+  });
+  describe("get user photo", () => {
+    it("should contain data if user have photo", (done) => {
+      chai
+        .request(app)
+        .get(`/api/user/photo/${regUser._id}`)
+        .end((err, res) => {
+          expect(res).to.have.nested.property(
+            "header.content-type",
+            "application/octet-stream"
+          );
+
+          done();
+        });
+    });
+    it("should not contain data if user does not have a photo", (done) => {
+      chai
+        .request(app)
+        .get(`/api/user/photo/${regUser2._id}`)
+        .end((err, res) => {
+          expect(res).to.not.have.nested.property("header.content-type");
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+  describe("follow user", () => {
+    it("should show 401 if user not logged in", (done) => {
+      chai
+        .request(app)
+        .put(`/api/user/follow`)
+        .send({
+          userId: regUser._id,
+          followId: regUser2._id,
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property(
+            "text",
+            '{"error":"Unauthorized Access!"}'
+          );
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
+    it("should 200 if user logged in", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user1@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .put(`/api/user/follow`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .send({
+              userId: regUser._id,
+              followId: regUser2._id,
+            })
+            .end((err, res) => {
               expect(res).to.have.nested.property(
-                "body.user.about",
-                'here'
+                "body.followers[0].name",
+                "janedoe"
+              );
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+  });
+  describe("unfollow user", () => {
+    it("should show 401 if user not logged in", (done) => {
+      chai
+        .request(app)
+        .put(`/api/user/unfollow`)
+        .send({
+          userId: regUser._id,
+          followId: regUser2._id,
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property(
+            "text",
+            '{"error":"Unauthorized Access!"}'
+          );
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
+    it("should 200 if user logged in", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user1@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .put(`/api/user/unfollow`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .send({
+              userId: regUser._id,
+              unfollowId: regUser2._id,
+            })
+            .end((err, res) => {
+              expect(res).to.not.have.nested.property(
+                "body.followers[0].name",
+                "janedoe"
+              );
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+  });
+  describe("delete user", () => {
+    it("should show 401 if user not logged in", (done) => {
+      chai
+        .request(app)
+        .delete(`/api/user/${regUser._id}`)
+        .end((err, res) => {
+          expect(res).to.have.nested.property(
+            "text",
+            '{"error":"Unauthorized Access!"}'
+          );
+          expect(res).to.have.status(401);
+          done();
+        });
+    });
+    it("should show 403 when delete by someone that is not the owner nor admin", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user2@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .delete(`/api/user/${regUser._id}`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .end((err, res) => {
+              expect(res).to.have.nested.property(
+                "text",
+                '{"error":"User is not authorized to perform this action"}'
+              );
+              expect(res).to.have.status(403);
+              done();
+            });
+        });
+    });
+    it("should show 200 when delete by admin", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user1@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .delete(`/api/user/${regUser._id}`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .send()
+            .end((err, res) => {
+              expect(res).to.have.nested.property(
+                "text",
+                '{"message":"User deleted successfully"}'
+              );
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+    it("should show 200 when delete by owner", (done) => {
+      chai
+        .request(app)
+        .post("/api/signin")
+        .send({
+          email: "user2@test.com",
+          password: "testuser",
+        })
+        .end((err, res) => {
+          expect(res).to.have.nested.property("body.token");
+          chai
+            .request(app)
+            .delete(`/api/user/${regUser2._id}`)
+            .set("Authorization", `Bearer ${res.body.token}`)
+            .send()
+            .end((err, res) => {
+              expect(res).to.have.nested.property(
+                "text",
+                '{"message":"User deleted successfully"}'
               );
               expect(res).to.have.status(200);
               done();
