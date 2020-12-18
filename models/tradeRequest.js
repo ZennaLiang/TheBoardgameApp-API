b207a0785f0c8c14a03091f4d3d47badc0d2bcf8
Louie Tse <28274608+tselouie@users.noreply.github.com>
2020-12-18 15:32:24 -0500

    added tags to tradeoffer/want

--- models/tradeRequest.js
+++ models/tradeRequest.js
@@ -2,67 +2,71 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
 const tradeRequestSchema = new mongoose.Schema({
-    tradeSender: { 
-        type: ObjectId, 
-        ref: "User" 
-    },
+  tradeSender: {
+    type: ObjectId,
+    ref: "User"
+  },
 
-    tradeReceiver: { 
-        type: ObjectId, 
-        ref: "User" 
-    },
+  tradeReceiver: {
+    type: ObjectId,
+    ref: "User"
+  },
 
-    tradeOffer: [{
-        id: { 
-            type: String, 
-            ref: "Boardgame" },
-        name:{
-            type: String
-        },
-        condition:{
-            type: String,
-            enum: ['Excellent','Good','Fair','Poor','N/A'],
-            default:'N/A'
-        },
-        price:{
-            type: Number
-        }
-        }],
-
-    tradeWants: [{
-        id: { 
-            type: String, 
-            ref: "Boardgame" },
-        name:{
-            type: String
-        },
-        condition:{
-            type: String,
-            enum: ['Excellent','Good','Fair','Poor','N/A'],
-            default:'N/A'
-        },
-        price:{
-            type: Number
-        }
-        }],
-    
-    notes: {
+  tradeOffer: [
+    {
+      id: {
         type: String,
-        required: true
-    },
-    status:{
+        ref: "Boardgame"
+      },
+      name: {
+        type: String
+      },
+      condition: {
         type: String,
-        enum:['Open', 'Closed', 'Pending'],
-        required: true,
-        default:'Open'
-    },
+        enum: ["Excellent", "Good", "Fair", "Poor", "N/A"],
+        default: "N/A"
+      },
+      price: {
+        type: Number
+      },
+      tags: [{ type: String, unique: true, default: [] }]
+    }
+  ],
 
-    createdDate: {
-        type: Date,
-        default: Date.now
-    },
-    updated: Date,
-   
+  tradeWants: [
+    {
+      id: {
+        type: String,
+        ref: "Boardgame"
+      },
+      name: {
+        type: String
+      },
+      condition: {
+        type: String,
+        enum: ["Excellent", "Good", "Fair", "Poor", "N/A"],
+        default: "N/A"
+      },
+      tags: [{ type: String, unique: true, default: [] }]
+    }
+  ],
+
+  notes: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ["Open", "Closed", "Pending"],
+    required: true,
+    default: "Open"
+  },
+
+  createdDate: {
+    type: Date,
+    default: Date.now
+  },
+  updated: Date
 });
 
 module.exports = mongoose.model("Trades", tradeRequestSchema);
